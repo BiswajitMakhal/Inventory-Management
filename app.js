17e1fbc020b859d512402967e5e2d391f96ecf00
BiswajitMakhal <makhalbiswajit1@gmail.com>
2026-03-04 05:51:12 +0530

    route update

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 app.use(express.static("public"));
 
-// app.use(cors())
+app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 const dbConnection = require("./app/config/db");
 dbConnection();
 
-const dashboard = async (req, res) => {
-  return res.render("dashboard");
-};
+// const dashboard = async (req, res) => {
+//   return res.render("dashboard");
+// };
 
-const Router = express.Router();
-Router.get("/dashboard", dashboard);
-app.use(Router);
+const dashboardRoute = require("./app/routes/dashboardRoute");
+app.use(dashboardRoute);
 
 const port = 7005;
 app.listen(port, () => {
